c89c524507d19a09045e7acaef51ba3e755c00d3
plan-max <rasatauid@gmail.com>
2026-05-09 08:06:12 +0800

    Delete js directory

--- js/service-worker-register.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/recovery-max/sw.js', { scope: '/recovery-max/' })
-      .catch(() => {});
-  });
-}
